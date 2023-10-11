d0ab19e5627cdcd09a522a37cdbe3505a79ddbab
varsha.zurange@inflectionzone.com <varsha.zurange@inflectionzone.com>
2023-10-11 17:00:03 +0530

    Reactivity and Logical blocks

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,58 @@
 <script>
-	export let name;
+	// import Greeting from "./components/greetings.svelte";
+	// export let name;
+	// let data = "Any data"
+
+	// let a = 0;
+
+	// function updateNum() {
+	// 	a = 24;
+	// };
+	// let firstName = 'John';
+	// let lastName = 'Doe';
+
+	// $: fullName = `${firstName} ${lastName}`
+	// console.log("fullName", fullName)
+
+		let count = 0;
+		function increment() {
+			count += 1;
+		}
+
+		let flowers = ['Jasmin', 'Hibiscus', 'Tulip'];
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<!-- <Greeting data = "Good evening"/>
+	<h1>Hello {name}!</h1> -->
+	
+	<!-- <p>My First name is {firstName}</p>
+	<p>My Last name is {lastName}</p>
+	<button on:click={ () => {firstName = "Varsha" ; lastName = "Zurange"}}> Update Name</button>
+	<p>{fullName}</p> -->
+
+	<button on:click={increment}>
+		Clicked {count}
+	</button>
+
+	<p>{count}</p>
+	{#if count > 7}
+		<p>{count} is greater than 7</p>
+
+	{:else if count < 5}
+		<p>{count} is less than 5</p>
+
+	{:else}
+		<p>{count} is between 5 to 7</p>
+	{/if}
+	
+	<p>Flowers are: </p>
+	{#each flowers as flower}
+		<h3>{flower}</h3>
+	{/each}
+
+	<!-- <p>{a}</p> -->
+	<!-- <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p> -->
 </main>
 
 <style>
@@ -15,12 +63,12 @@
 		margin: 0 auto;
 	}
 
-	h1 {
+	/* h1 {
 		color: #ff3e00;
 		text-transform: uppercase;
 		font-size: 4em;
 		font-weight: 100;
-	}
+	} */
 
 	@media (min-width: 640px) {
 		main {
---
+++ src/components/greetings.svelte
@@ -0,0 +1,18 @@
+<script>
+    export let data
+
+    let arr = 0;
+    
+    function clickIt() {
+        arr += 1; 
+    }
+
+</script>
+
+    <h1>Welcome to svelte learning</h1>
+    <button on:click = {clickIt}> 
+        Clicked {arr}
+        {arr === 1 ? 'time' : 'times'}
+
+    </button>
+    <div>{data}</div>
